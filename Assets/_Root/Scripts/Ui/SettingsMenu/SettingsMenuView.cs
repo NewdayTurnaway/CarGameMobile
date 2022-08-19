989edfecc0387cadbb5599ba6623655493009f5c
NewdayTurnaway <moba545@gmail.com>
2022-08-19 21:43:16 +0300

    Add MainMenu Localization

--- Assets/_Root/Scripts/Ui/SettingsMenu/SettingsMenuView.cs
+++ Assets/_Root/Scripts/Ui/SettingsMenu/SettingsMenuView.cs
@@ -6,16 +6,24 @@ namespace Ui
 {
     internal sealed class SettingsMenuView : MonoBehaviour
     {
-        [SerializeField] private Button _buttonReturnMenu;
+        [SerializeField] private Button _returnMenuButton;
+        
+        [Header("Language")]
+        [SerializeField] private Button _enLanguageButton;
+        [SerializeField] private Button _ruLanguageButton;
 
-        public void Init(UnityAction returnMenu)
+        public void Init(UnityAction enLanguange, UnityAction ruLanguange, UnityAction returnMenu)
         {
-            _buttonReturnMenu.onClick.AddListener(returnMenu);
+            _enLanguageButton.onClick.AddListener(enLanguange);
+            _ruLanguageButton.onClick.AddListener(ruLanguange);
+            _returnMenuButton.onClick.AddListener(returnMenu);
         }
 
         public void OnDestroy()
         {
-            _buttonReturnMenu.onClick.RemoveAllListeners();
+            _enLanguageButton.onClick.RemoveAllListeners();
+            _ruLanguageButton.onClick.RemoveAllListeners();
+            _returnMenuButton.onClick.RemoveAllListeners();
         }
     }
 }
